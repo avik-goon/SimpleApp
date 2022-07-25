a4c6c97cfa3ca7dd337024e907868b98251af204
avikgoon <avikgoon@matrixnmedia.com>
2022-07-26 00:28:05 +0530

    Toast added

--- lib/utils/login_user.dart
+++ lib/utils/login_user.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: avoid_print, use_build_context_synchronously
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_app/model/auth/login_model.dart';
 import 'package:simple_app/screens/dashboard.dart';
@@ -15,12 +15,33 @@ class LoginUser {
           email: userLoginDetails.getEmail ?? '',
           password: userLoginDetails.getPassword ?? '');
       print(credential.user);
+      Fluttertoast.showToast(
+          msg: 'Login Successful',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          // timeInSecForIos: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.yellow);
       Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: (context) => const Dashboard()));
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
+        Fluttertoast.showToast(
+            msg: 'No user found for that email.',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            // timeInSecForIos: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.yellow);
         print('No user found for that email.');
       } else if (e.code == 'wrong-password') {
+        Fluttertoast.showToast(
+            msg: 'Wrong password provided for that user.',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            // timeInSecForIos: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.yellow);
         print('Wrong password provided for that user.');
       }
     }
--- lib/utils/register_user.dart
+++ lib/utils/register_user.dart
@@ -1,11 +1,24 @@
 // ignore_for_file: avoid_print
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:simple_app/model/auth/signup_model.dart';
 
 class RegisterUser {
   final UserSignUp userSignUpDetails;
   const RegisterUser({required this.userSignUpDetails});
+  void createToast(msg) {
+    print(msg);
+    Fluttertoast.showToast(
+        msg: msg,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        // timeInSecForIos: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.yellow);
+  }
+
   Future addUserToFirebase() async {
     try {
       final credential =
@@ -13,12 +26,12 @@ class RegisterUser {
         email: userSignUpDetails.getEmail,
         password: userSignUpDetails.getPassword,
       );
-      print(credential.user?.uid);
+      createToast(credential.user?.uid);
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
-        print('The password provided is too weak.');
+        createToast('The password provided is too weak.');
       } else if (e.code == 'email-already-in-use') {
-        print('The account already exists for that email.');
+        createToast('The account already exists for that email.');
       }
     } catch (e) {
       print(e);
